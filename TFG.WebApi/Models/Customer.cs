58490e4d08af315e7dcaa1f88cfc17918bddb42c
Adriel Mariappen <adriel@softidoc.co.za>
2022-06-29 16:16:28 +0200

    Created customer model.

---
+++ TFG.WebApi/Models/Customer.cs
@@ -0,0 +1,18 @@
+﻿namespace TFG.WebApi.Models
+{
+    public class Customer
+    {
+        public int CustomerId { get; set; }
+
+        public string Name { get; set; }
+
+        public string Description { get; set; }
+
+        public string Location { get; set; }
+
+        public string EmailAddress { get; set; }
+
+        public string ContactNumber { get; set; }
+
+    }
+}
