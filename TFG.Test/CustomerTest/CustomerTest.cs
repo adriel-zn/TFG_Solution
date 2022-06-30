879a41e69733f89413bdd160d2b5f867830c50ae
Adriel Mariappen <adriel@softidoc.co.za>
2022-06-30 09:53:47 +0200

    Added test for customer

---
+++ TFG.Test/CustomerTest/CustomerTest.cs
@@ -0,0 +1,77 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Net;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Http;
+
+namespace TFG.Test.CustomerTest
+{
+    [TestClass]
+    public class CustomerTest
+    {
+        [TestMethod]
+        public async Task Test_GetTokenAuthentication()
+        {
+            var config = new HttpConfiguration();
+            config.MapHttpAttributeRoutes();
+
+            using (var server = new HttpServer(config))
+            {
+
+                var client = new HttpClient(server);
+
+                string url = "https://localhost:44330/api/v2/users/authenticate";
+
+                var request = new HttpRequestMessage
+                {
+                    RequestUri = new Uri(url),
+                    Method = HttpMethod.Post,
+                    Content = new StringContent("{\"username\":\"Username1\",\"password\":\"Password1\"}", Encoding.UTF8, "application/json")
+                };
+
+                request.Headers.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+
+                using (var response = await client.SendAsync(request))
+                {
+                    var test = response;
+                }
+            }
+        }
+
+        [TestMethod]
+        public async Task  Test_GetAllCustomer()
+        {
+            //Arrange
+            var config = new HttpConfiguration();
+            //configure web api
+            config.MapHttpAttributeRoutes();
+
+            //Act
+            using (var server = new HttpServer(config))
+            {
+
+                var client = new HttpClient(server);
+
+                string url = "https://localhost:44330/api/v2/customers/";
+
+                var request = new HttpRequestMessage
+                {
+                    RequestUri = new Uri(url),
+                    Method = HttpMethod.Get
+                };
+
+                request.Headers.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+
+                using (var response = await client.SendAsync(request))
+                {
+                    //Assert
+                    Assert.AreEqual(HttpStatusCode.OK, response.StatusCode);
+                }
+            }
+
+        }
+    }
+}
---
+++ TFG.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("TFG.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("TFG.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2022")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("1159aa36-67b3-45db-97e9-8510850c96d3")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
